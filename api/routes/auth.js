418e1e3a49ead279d1f9e4ecc5b7539a29a16f03
Sofia <sraissiguier@gmail.com>
2021-11-28 16:42:40 -0300

    Create auth routes

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,42 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //REGISTER
+router.post("/register", async (req,res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password : CryptoJS.AES.encrypt(req.body.password, process.env.PASSW_SECRET).toString()
+    });
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(200).json(savedUser);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+//LOGIN
+router.post("/login", async (req,res) => {
+    try {
+        const user = await User.findOne({username: req.body.username})
+        !user && res.status(401).json("Wrong credentials");
+        
+        const hashedPassw = CryptoJS.AES.decrypt(user.password, process.env.PASSW_SECRET);
+        const passw = hashedPassw.toString(CryptoJS.enc.Utf8);
+        passw !== req.body.password && res.status(401).json("Wrong credentials");
+
+        //destructure of user object to send user info but the password (security purpose)
+        const { password, ...others } = user._doc;
+
+        res.status(200).json(others);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+
 
 module.exports = router;
