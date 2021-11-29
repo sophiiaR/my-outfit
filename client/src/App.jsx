7230c1c4d67ef127a189076156dee0907ae78243
Sofia <sraissiguier@gmail.com>
2021-11-29 17:29:14 -0300

    Add routes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,37 @@ import Product from './pages/Product';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
   return (
-    <Cart />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/success">
+        </Route>
+        <Route path="/login"><Login /></Route>
+        <Route path="/register">
+          
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
