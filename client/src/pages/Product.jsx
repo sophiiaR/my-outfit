00cb41c36ff1d19244c8047c0bb45b9c536270e7
Sofia <sraissiguier@gmail.com>
2021-12-16 17:54:27 -0300

    Add function to add product to cart

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -8,6 +8,9 @@ import { mobile } from "../responsive";
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { publicRequest } from "../requestMethods";
+import Navbar from '../components/Navbar';
+import { addProduct } from '../redux/cartRedux';
+import { useDispatch } from 'react-redux';
 
 const Container = styled.div``;
 
@@ -128,6 +131,7 @@ const Product = () => {
     const [quantity, setQuantity] = useState(1);
     const [color, setColor] = useState("");
     const [size, setSize] = useState("");
+    const dispatch = useDispatch();
    
 
     useEffect(() => {
@@ -148,10 +152,17 @@ const Product = () => {
         }
     };
 
+    const handleClick = () => {
+        dispatch(
+            addProduct({...product, quantity, color, size})
+        )
+    }
+
 
     return (
         <Container>
             <Announcement />
+            <Navbar />
             <Wrapper>
                 <ImageContainer>
                     <Image src={product.img}/>
@@ -159,7 +170,7 @@ const Product = () => {
                 <InfoContainer>
                     <Title>{product.title}</Title>
                     <Desc>{product.desc}</Desc>
-                    <Price>{product.price}</Price>
+                    <Price>$ {product.price}</Price>
                     <FilterContainer>
                         <Filter>
                             <FilterTitle>Color</FilterTitle>
@@ -182,7 +193,7 @@ const Product = () => {
                                 <Amount>{quantity}</Amount>
                             <Add onClick={() => handleQuantity("inc")}/>
                         </AmountContainer>
-                        <Button>ADD TO CART</Button>
+                        <Button onClick={handleClick}>ADD TO CART</Button>
                     </AddContainer>
                 </InfoContainer>
             </Wrapper>
