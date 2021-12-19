ed05c5254158064f55c61feeb06b2cda3ba5e05c
Sofia <sraissiguier@gmail.com>
2021-12-19 13:13:48 -0300

    Add login and register redirection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,9 +12,13 @@ import {
   Redirect,
 } from "react-router-dom";
 import Success from './pages/Success';
+import { useSelector } from "react-redux";
 
 
 const App = () => {
+
+  const user = useSelector((state) => state.user.currentUser);
+
   return (
     <Router>
       <Switch>
@@ -33,9 +37,11 @@ const App = () => {
         <Route path="/success">
           <Success />
         </Route>
-        <Route path="/login"><Login /></Route>
+        <Route path="/login">
+          {user ? <Redirect to="/" /> : <Login />}
+        </Route>
         <Route path="/register">
-          
+          {user ? <Redirect to="/" /> : <Register />}
         </Route>
       </Switch>
     </Router>
