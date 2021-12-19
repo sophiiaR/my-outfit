b52162c77d1f47b00e83bc8d942c9d64841e904b
Sofia <sraissiguier@gmail.com>
2021-12-19 12:29:50 -0300

    Create api call for login process

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { mobile } from "../responsive";
+import { login } from "../redux/apiCalls";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Container = styled.div`
     width: 100vw;
@@ -64,21 +67,40 @@ const Link = styled.a`
     cursor: pointer;
 `;
 
-// const Error = styled.span`
-//     color: red;
-// `;
+const Error = styled.span`
+    color: red;
+`;
 
 const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+    const { isFetching, error } = useSelector((state) => state.user);
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        login(dispatch, { username, password });
+    };
+
     return (
         <Container>
             <Wrapper>
                 <Title>SIGN IN</Title>
                 <Form>
-                    <Input placeholder="Username"/>
-                    <Input placeholder="Password"/>
-                    <Button>
+                    <Input 
+                        placeholder="Username"
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <Input 
+                        placeholder="Password"
+                        type= "password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button onClick={handleClick} disabled={isFetching}>
                         LOGIN
                     </Button>
+                    {error && <Error>Something went wrong...</Error>}
                     <Link>FORGOT PASSWORD?</Link>
                     <Link>CREATE A NEW ACCOUNT</Link>
                 </Form>
